a14f3dea053fecc86c5e653aded744acf7f94f68
rameem2003 <rameem2019@gmail.com>
2022-07-17 21:46:37 +0600

    Calculator windows app

---
+++ src/java_gui_calculator/Java_gui_calculator.java
@@ -0,0 +1,14 @@
+
+package java_gui_calculator;
+
+public class Java_gui_calculator {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        calculator app = new calculator();
+        
+        app.show();
+    }
+    
+}
